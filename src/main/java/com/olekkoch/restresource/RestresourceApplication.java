da520f991415a1daaab05b15736a45fd7905a59b
alex <alex@phpDeb.local>
2022-05-20 16:38:50 +0200

    Return random number class

--- src/main/java/com/olekkoch/restresource/RestresourceApplication.java
+++ src/main/java/com/olekkoch/restresource/RestresourceApplication.java
@@ -9,5 +9,4 @@ public class RestresourceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestresourceApplication.class, args);
 	}
-
 }
--- src/main/java/com/olekkoch/restresource/controllers/RandomController.java
+++ src/main/java/com/olekkoch/restresource/controllers/RandomController.java
@@ -2,23 +2,25 @@ package com.olekkoch.restresource.controllers;
 import java.util.HashMap;
 import java.util.Random;
 
+import com.olekkoch.restresource.service.RandomNumber;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RandomController {
-    private HashMap<Integer, Integer> hashNums = new HashMap<>();
+    private HashMap<Integer, RandomNumber> randomNumberResources = new HashMap<>();
 	private Random rand = new Random();
 	
 	@GetMapping("/")
-	public int getRandomNumbers(@RequestParam(value="id", defaultValue= "1") int id) {
-			if(!hashNums.containsKey(id)) {
-				hashNums.put(id, rand.nextInt());
-				return hashNums.get(id);
+	public RandomNumber getRandomNumbers(@RequestParam(value="id", defaultValue= "1") int id) {
+			if(!randomNumberResources.containsKey(id)) {
+				randomNumberResources.put(id, new RandomNumber(id, rand.nextInt()));
+				return randomNumberResources.get(id);
 			}
 			else {
-				return hashNums.get(id);
+				return randomNumberResources.get(id);
 			}
 	}
 }
