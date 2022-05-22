3a0243c73fa751819258b65b481f60e032a6f516
alexkochetov <kchetovalex@yahoo.com>
2022-05-23 01:48:46 +0200

    Delete unnecessary comments

--- src/main/java/com/olekkoch/restresource/controllers/RandomController.java
+++ src/main/java/com/olekkoch/restresource/controllers/RandomController.java
@@ -1,7 +1,5 @@
 package com.olekkoch.restresource.controllers;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Random;
 
 import com.olekkoch.restresource.model.RandomNumber;
 import com.olekkoch.restresource.service.RandomNumberService;
@@ -9,12 +7,10 @@ import com.olekkoch.restresource.service.RandomNumberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RandomController {
-//    private HashMap<Integer, RandomNumber> randomNumberResources = new HashMap<>();
 	
 	@Autowired
 	private RandomNumberService randomNumberService;
@@ -27,7 +23,6 @@ public class RandomController {
 	@GetMapping("/{id}")
 	public RandomNumber getRandomNumberById(@PathVariable(value="id") int id) {
 		RandomNumber randomNumber = randomNumberService.getNumerById(id);
-//		randomNumberService.saveNewResource(randomNumber);
 		return randomNumber;
 	}
 }
